0badf2d5484e666fe8ebf9f963bc8a64b1840cfe
AminaBezza <bezzaamina31@gmail.com>
2025-05-31 18:23:43 +0100

    modification

--- app/src/main/java/org/example/model/rayon.java
+++ app/src/main/java/org/example/model/rayon.java
@@ -1,4 +1,45 @@
 package org.example.model;
 
 public class rayon {
+    private int id;
+    private String nom;
+    private String description;
+
+    public rayon(int id, String nom, String description) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Override
+    public String toString() {
+        return "Rayon{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/org/example/model/vente.java
+++ app/src/main/java/org/example/model/vente.java
@@ -1,4 +1,68 @@
 package org.example.model;
 
 public class vente {
+    private int id;
+    private String date;
+    private double montantTotal;
+    private String modePaiement;
+    private int clientId;
+
+    public vente(int id, String date, double montantTotal, String modePaiement, int clientId) {
+        this.id = id;
+        this.date = date;
+        this.montantTotal = montantTotal;
+        this.modePaiement = modePaiement;
+        this.clientId = clientId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getMontantTotal() {
+        return montantTotal;
+    }
+
+    public void setMontantTotal(double montantTotal) {
+        this.montantTotal = montantTotal;
+    }
+
+    public String getModePaiement() {
+        return modePaiement;
+    }
+
+    public void setModePaiement(String modePaiement) {
+        this.modePaiement = modePaiement;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    @Override
+    public String toString() {
+        return "Vente{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", montantTotal=" + montantTotal +
+                ", modePaiement='" + modePaiement + '\'' +
+                ", clientId=" + clientId +
+                '}';
+    }
 }
