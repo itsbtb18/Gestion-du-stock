3be08f850b980805650c94cf61406c566757415e
Bettayeb Mohamed Aimen <ma.bettayeb@esi-sba.dz>
2025-05-31 18:17:48 +0100

    implementing model

--- app/src/main/java/org/example/model/Fournisseur.java
+++ app/src/main/java/org/example/model/Fournisseur.java
@@ -1,4 +1,47 @@
 package org.example.model;
 
 public class Fournisseur {
+    private int id;
+    private String nom;
+    private String adresse;
+    private String telephone;
+
+    public Fournisseur(int id, String nom, String adresse, String telephone) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.telephone = telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
--- app/src/main/java/org/example/model/produit.java
+++ app/src/main/java/org/example/model/produit.java
@@ -1,4 +1,82 @@
 package org.example.model;
 
+import java.util.Date;
+
 public class produit {
+    private categorie categorie;
+    private double prixAchat;
+    private double prixVente;
+    private Date dateExpiration;
+    private String codeBar;
+    private String nom;
+    private Fournisseur fournisseur;
+
+    public produit() {
+    }
+
+    public produit(categorie categorie, double prixAchat, double prixVente, Date dateExpiration, String codeBar, String nom, Fournisseur fournisseur) {
+        this.categorie = categorie;
+        this.prixAchat = prixAchat;
+        this.prixVente = prixVente;
+        this.dateExpiration = dateExpiration;
+        this.codeBar = codeBar;
+        this.nom = nom;
+        this.fournisseur = fournisseur;
+    }
+
+    public categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public double getPrixAchat() {
+        return prixAchat;
+    }
+
+    public void setPrixAchat(double prixAchat) {
+        this.prixAchat = prixAchat;
+    }
+
+    public double getPrixVente() {
+        return prixVente;
+    }
+
+    public void setPrixVente(double prixVente) {
+        this.prixVente = prixVente;
+    }
+
+    public Date getDateExpiration() {
+        return dateExpiration;
+    }
+
+    public void setDateExpiration(Date dateExpiration) {
+        this.dateExpiration = dateExpiration;
+    }
+
+    public String getCodeBar() {
+        return codeBar;
+    }
+
+    public void setCodeBar(String codeBar) {
+        this.codeBar = codeBar;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Fournisseur getFournisseur() {
+        return fournisseur;
+    }
+
+    public void setFournisseur(Fournisseur fournisseur) {
+        this.fournisseur = fournisseur;
+    }
 }
